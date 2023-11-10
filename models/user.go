7181b170eaf7ecd9856551109466eaecae5988ba
palmcm <palmermonkeyboy@gmail.com>
2023-11-10 21:23:29 +0700

    return token

--- models/user.go
+++ models/user.go
@@ -75,8 +75,6 @@ func LoginCheck(email string, password string) (string, error) {
 		return "", err
 	}
 
-	return "randomtoken", nil
-
 	token, err := token.GenerateToken(u.UserId)
 
 	if err != nil {
